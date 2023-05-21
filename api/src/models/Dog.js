d3061feb7475a36b59616f3ebb773ab741fb4acd
mateomohr02 <mohr.mateo@gmail.com>
2023-05-21 20:16:29 -0300

    functional back

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    weight:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    image: {
+      type: DataTypes.TEXT
+    },
+
+    lifeLength:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    isInDB: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    
+  }, {timestamps: false});
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const {Dog, Temperament} = require("../db.js")
+const axios = require('axios')
+const {key} = process.env
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -8,5 +11,107 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+
+const buscarDatos = async () => {
+    try {
+        const dogsApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${key}`);
+
+        const dogsDB = await Dog.findAll();
+
+        const allDogs = [...dogsApi.data, ...dogsDB]
+
+        return allDogs
+    } catch (error) {
+
+        throw new Error('Error al obtener los datos combinados')
+    
+    }
+}
+
+
+//RUTA BUSQUEDA NOMBRE CON QUERY
+router.get('/dogs/name?', async (req, res) => {
+    try {
+        const {name} = req.query
+        const allBreeds = await buscarDatos();        
+        const dog = allBreeds?.find(d => d.name.toLowerCase().includes(name.toLowerCase()))
+        
+        if (dog){
+            res.status(200).json(dog)
+        }else{
+            res.status(404).json({ message: "No se encontró la raza especificada" })
+        }
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+//RUTA BUSQUEDA ID
+router.get('/dogs/:idRaza', async (req, res) => {
+    try{
+        const { idRaza } = req.params
+        const allBreeds = await buscarDatos();
+        const dog = allBreeds.filter(d => d.id == idRaza )
+        if (dog){
+            res.status(200).json(dog)
+        }
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+}) 
+
+// CREAR NUEVO PERRO
+router.post('/dogs', async (req, res) => {
+    try {
+        const { name, weight, height, image, lifeLength } = req.body
+        const newDog = await Dog.create({
+            name,
+            weight,
+            height,
+            image,
+            lifeLength,
+        })
+
+        res.status(200).json(`El perro ${newDog.name} se creo correctamente.`)
+
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+// RUTA GET ALL DOGS
+router.get('/dogs', async (req,res) => {
+    try {
+        const Breeds = await buscarDatos()
+
+        res.status(200).json(Breeds)
+        
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+//RUTA TEMPERAMENTOS
+router.get('/temperaments', async (req, res) => {    
+    try{
+        const allTemperaments = await buscarDatos();
+        const allTemps = allTemperaments.map(d => d.temperament)
+        const filteredTemps = allTemps.toString().split(',');
+        filteredTemps.forEach(t => {
+            let temp = t.trim()
+            Temperament.findOrCreate({
+                where: {name: temp}
+            })
+        });
+
+        const temperaments = await Temperament.findAll();
+        res.status(200).json(temperaments)
+    
+    }catch(error){
+        res.status(400).json({error: error.message})
+    }
+    
+});
 
 module.exports = router;
