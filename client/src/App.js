e611c5e1ef7736af564a4f090239521724c6e0d1
mateomohr02 <mohr.mateo@gmail.com>
2023-05-22 11:31:30 -0300

    Routes front working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {Routes, Route, useLocation} from 'react-router-dom';
 
+import Home from './components/home.jsx';
+import Form from './components/form.jsx';
+import LandingPage from './components/landingPage';
+import NavBar from './components/navBar.jsx'
+import Detail from './components/detail.jsx'
 function App() {
+
+  const location = useLocation()
+
   return (
     <div className="App">
-      <h1>MATEO PIJA GRANDE</h1>
+        {location.pathname === '/' ? <LandingPage/> : <NavBar/>}
+      <Routes>
+        <Route exact path="/home" element={<Home/>}/>
+        <Route exact path="/create" element={<Form/>} />
+        <Route exact path='/detail' element={<Detail/>} />
+      </Routes>
+
     </div>
   );
 }
---
+++ client/src/components/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+        <h1>Soy el nb</h1>
+    </div>
+  );
+};
+
+export default NavBar;
